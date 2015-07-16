2e37f6b81c04e6b57e776139d8f746f68a478524
Tuan-Tu Tran <tuantu.t@gmail.com>
2015-07-16 21:22:54 +0200

    use unique option to insert data

--- import.py
+++ import.py
@@ -12,18 +12,12 @@ parser=argparse.ArgumentParser(description=__doc__, formatter_class=argparse.Arg
 parser.add_argument("worksFile", help="the csv file containing the raw works export from ProEco", metavar="CSV_FILE" , default="travaux.csv", nargs="?")
 parser.add_argument("-v","--verbose", action="store_const", dest="logging", const=logging.DEBUG, default=logging.INFO, help="show debug logs")
 parser.add_argument("--dsn", help="the dsn to use for db operations", action="store", dest="dsn", default="theros_dev")
-parser.add_argument("--is", "--insert-student", help="insert (ignoring duplicates) students into database (requires --dsn)", action="store_true", dest="insertStudents")
-parser.add_argument("--ic", "--insert-class", help="insert (ignoring duplicates) classes into database (requires --dsn)", action="store_true", dest="insertClasses")
-parser.add_argument("--iw", "--insert-work", help="insert (ignoring duplicates) works into database (requires --dsn)", action="store_true", dest="insertWorks")
+parser.add_argument("-i", "--insert", help="insert (ignoring duplicates) data into database (see --dsn)", action="store_true", dest="insertData")
 args=parser.parse_args()
 logging.basicConfig(level=args.logging, stream=sys.stdout)
 logger=logging.getLogger()
 
-if not args.dsn and (
-        args.insertStudents
-        or args.insertClasses
-        or args.insertWorks
-    ):
+if not args.dsn and args.insertData:
     parser.error("missing --dsn specification")
 
 worksFile=args.worksFile
@@ -59,46 +53,44 @@ with open(worksFile) as fh:
 
 logger.info("got %i works, %i students, %i classes", len(works), len(students), len(classes))
 
-if args.insertStudents or args.insertClasses or args.insertWorks:
+if args.insertData:
     logging.info("connecting to database")
     import pyodbc
     conn=pyodbc.connect(dsn=args.dsn)
     try:
         db=conn.cursor()
-        if args.insertStudents:
-            logging.info("inserting students")
-            params=[(s,) for s in sorted(students)]
-            db.executemany("INSERT IGNORE INTO student(st_name) VALUES (?)", params)
 
-        if args.insertClasses:
-            logging.info("inserting classes")
-            params=[(c,) for c in sorted(classes)]
-            db.executemany("INSERT IGNORE INTO class(cl_desc) VALUES (?)", params)
+        logging.info("inserting students")
+        params=[(s,) for s in sorted(students)]
+        db.executemany("INSERT IGNORE INTO student(st_name) VALUES (?)", params)
 
-        if args.insertWorks:
-            logging.info("inserting works")
-            classes=db.execute("SELECT * FROM class").fetchall()
-            classes=dict([(c.cl_desc, c.cl_id) for c in classes])
-            students=db.execute("SELECT * FROM student").fetchall()
-            students=dict([(s.st_name, s.st_id) for s in students])
-            query = "INSERT IGNORE INTO raw_data(rd_st_id, rd_cl_id, rd_desc) VALUES (?,?,?)"
-            params=[]
-            for w in works:
-                if w.klass not in classes:
-                    logger.error("no such class: %s for work at line %i",w.klass, w.line)
-                    continue
+        logging.info("inserting classes")
+        params=[(c,) for c in sorted(classes)]
+        db.executemany("INSERT IGNORE INTO class(cl_desc) VALUES (?)", params)
 
-                if w.student not in students:
-                    logger.error("no such student: %s for work at line %i",w.student, w.line)
-                    continue
+        logging.info("inserting works")
+        classes=db.execute("SELECT * FROM class").fetchall()
+        classes=dict([(c.cl_desc, c.cl_id) for c in classes])
+        students=db.execute("SELECT * FROM student").fetchall()
+        students=dict([(s.st_name, s.st_id) for s in students])
+        query = "INSERT IGNORE INTO raw_data(rd_st_id, rd_cl_id, rd_desc) VALUES (?,?,?)"
+        params=[]
+        for w in works:
+            if w.klass not in classes:
+                logger.error("no such class: %s for work at line %i",w.klass, w.line)
+                continue
 
-                classId = classes[w.klass]
-                studentId = students[w.student]
-                params.append((studentId, classId, w.desc))
-            if len(params):
-                db.executemany(query, params)
-            else:
-                logger.warn("no work inserted")
+            if w.student not in students:
+                logger.error("no such student: %s for work at line %i",w.student, w.line)
+                continue
+
+            classId = classes[w.klass]
+            studentId = students[w.student]
+            params.append((studentId, classId, w.desc))
+        if len(params):
+            db.executemany(query, params)
+        else:
+            logger.warn("no work inserted")
         conn.commit()
     finally:
         conn.close()
