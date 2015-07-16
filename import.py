7f34f717863836e8bd541ed8d37c23f8ca6583f2
Tuan-Tu Tran <tuantu.t@gmail.com>
2015-07-16 21:35:56 +0200

    implement insert of classes composition

--- import.py
+++ import.py
@@ -13,6 +13,7 @@ parser.add_argument("worksFile", help="the csv file containing the raw works exp
 parser.add_argument("-v","--verbose", action="store_const", dest="logging", const=logging.DEBUG, default=logging.INFO, help="show debug logs")
 parser.add_argument("--dsn", help="the dsn to use for db operations", action="store", dest="dsn", default="theros_dev")
 parser.add_argument("-i", "--insert", help="insert (ignoring duplicates) data into database (see --dsn)", action="store_true", dest="insertData")
+parser.add_argument("-y", "--shoolyear", help="specify the current school year", action="store", dest="schoolyear", default="2014-15")
 args=parser.parse_args()
 logging.basicConfig(level=args.logging, stream=sys.stdout)
 logger=logging.getLogger()
@@ -24,6 +25,7 @@ worksFile=args.worksFile
 works=[]
 classes=set()
 students=set()
+compositions=[]
 class Work:
     def __init__(self, klass, student, desc, line):
         self.klass=klass
@@ -45,8 +47,9 @@ with open(worksFile) as fh:
         student=student.replace("  "," ")
         classes.add(klass)
         students.add(student)
+        compositions.append(Work(klass, student, desc, i+1))
         if desc:
-            works.append(Work(klass, student, desc, i+1))
+            works.append(compositions[-1])
             logger.debug("keep %s", works[-1])
         else:
             logger.debug("discarded line %s", line.strip())
@@ -68,11 +71,18 @@ if args.insertData:
         params=[(c,) for c in sorted(classes)]
         db.executemany("INSERT IGNORE INTO class(cl_desc) VALUES (?)", params)
 
-        logging.info("inserting works")
+        logging.info("inserting classes compositions")
+        db.execute("INSERT IGNORE INTO schoolyear(sy_desc) VALUES (?)", (args.schoolyear,))
+        schoolyear = db.execute("SELECT sy_id FROM schoolyear WHERE sy_desc = ?", (args.schoolyear,)).fetchone().sy_id
         classes=db.execute("SELECT * FROM class").fetchall()
         classes=dict([(c.cl_desc, c.cl_id) for c in classes])
         students=db.execute("SELECT * FROM student").fetchall()
         students=dict([(s.st_name, s.st_id) for s in students])
+        db.executemany("INSERT IGNORE INTO student_class(sc_st_id, sc_cl_id, sc_sy_id) VALUES (?,?,?)",[
+            (students[c.student], classes[c.klass], schoolyear) for c in compositions
+        ])
+
+        logging.info("inserting works")
         query = "INSERT IGNORE INTO raw_data(rd_st_id, rd_cl_id, rd_desc) VALUES (?,?,?)"
         params=[]
         for w in works:
