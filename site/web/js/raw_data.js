e96abecea10b0dad01909555db2b3321fcb57301
Tuan-Tu Tran <tuantu.t@gmail.com>
2015-08-19 03:21:28 +0200

    highlight select raw data

--- site/web/js/raw_data.js
+++ site/web/js/raw_data.js
@@ -2,6 +2,10 @@ var detailsUrl;
 
 $(function(){
     $("div#raw_data tr").click(function(){
+        if(!$(this).hasClass("selected")){
+            $("tr.selected").removeClass("selected");
+            $(this).addClass("selected");
+        }
         var id=$(this).find("input").val();
         $.get(detailsUrl,{id:id}, function(data){
             $("div#details").html(data);
