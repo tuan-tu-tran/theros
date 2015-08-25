568ae3cde3898858532d924710bba9c2992e9786
Tuan-Tu Tran <tuantu.t@gmail.com>
2015-08-25 22:19:52 +0200

    added a LoginController class

---
+++ site/src/AppBundle/Controller/LoginController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class LoginController extends Controller
+{
+    public function indexAction()
+    {
+    }
+}
