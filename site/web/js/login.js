164f7fef3029c56678a5eddbbbab9b28e1c1f94b
Tuan-Tu Tran <tuantu.t@gmail.com>
2015-08-26 01:23:38 +0200

    add client side validation on submit

--- site/web/js/login.js
+++ site/web/js/login.js
@@ -1,5 +1,14 @@
 $(function(){
     $("#cbTeacher").prop("selectedIndex", -1).combobox();
-    $("#bSubmit").button();
+    $("#bSubmit").button().click(function(){
+        var teacher=$("#cbTeacher").val();
+        if (!teacher) {
+            alert("Veuillez entrer votre login");
+            return false;
+        } else if (!$("#tbPassword").val()) {
+            alert("Veuillez entrer votre mot de passe");
+            return false;
+        }
+    });
     $("body").show();
 });
