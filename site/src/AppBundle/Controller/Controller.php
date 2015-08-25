6b58ebc81478b29254ce7ee9301b31efb1676122
Tuan-Tu Tran <tuantu.t@gmail.com>
2015-08-25 22:19:40 +0200

    added a Controller base class

---
+++ site/src/AppBundle/Controller/Controller.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller as sfController;
+
+/**
+ * Base class for all controllers
+ */
+abstract class Controller extends sfController
+{
+    /**
+     * Return a database connection.
+     * Shorthand to get the database_connection service
+     */
+    protected function getDb()
+    {
+        return $this->get("database_connection");
+    }
+}
