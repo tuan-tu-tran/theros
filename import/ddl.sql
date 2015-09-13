9bd931dc4926ada3ed426ae1cd8497cce7948b55
Tuan-Tu Tran <tuantu.t@gmail.com>
2015-09-14 00:49:13 +0200

    add role/teacher_role tables

--- import/ddl.sql
+++ import/ddl.sql
@@ -63,6 +63,21 @@ CREATE TABLE `raw_data_work` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role` (
+  `ro_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ro_role` varchar(45) NOT NULL,
+  PRIMARY KEY (`ro_id`),
+  UNIQUE KEY `ro_role_UNIQUE` (`ro_role`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `schoolyear`
 --
 
@@ -145,6 +160,25 @@ CREATE TABLE `teacher` (
   `tea_pwd_changed` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'whether passwd was initd (#12)',
   PRIMARY KEY (`tea_id`),
   UNIQUE KEY `tea_fullname_UNIQUE` (`tea_fullname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `teacher_role`
+--
+
+DROP TABLE IF EXISTS `teacher_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `teacher_role` (
+  `tr_id` int(11) NOT NULL AUTO_INCREMENT,
+  `tr_tea_id` int(11) NOT NULL,
+  `tr_ro_id` int(11) NOT NULL,
+  PRIMARY KEY (`tr_id`),
+  UNIQUE KEY `index2` (`tr_tea_id`,`tr_ro_id`),
+  KEY `fk_teacher_role_2_idx` (`tr_ro_id`),
+  CONSTRAINT `fk_teacher_role_2` FOREIGN KEY (`tr_ro_id`) REFERENCES `role` (`ro_id`),
+  CONSTRAINT `fk_teacher_role_1` FOREIGN KEY (`tr_tea_id`) REFERENCES `teacher` (`tea_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
