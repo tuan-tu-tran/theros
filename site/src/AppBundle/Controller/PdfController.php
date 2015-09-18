a658fff77d6726e076199bbcc2bfd3e49d339d45
Tuan-Tu Tran <tuantu.t@gmail.com>
2015-09-19 00:54:26 +0200

    indent

--- site/src/AppBundle/Controller/PdfController.php
+++ site/src/AppBundle/Controller/PdfController.php
@@ -140,44 +140,44 @@ class PdfController extends Controller implements IAdminPage
         $contentWidth=$pageWidth - 2*$rightMargin;
         $height=5;
         if ($what == self::ALL || $what == self::TUTORS) {
-        $pdf->AddPage();
-        $pdf->Image($this->webDir("img/logo.png"), $pdf->getX(), $pdf->getY(), 20, 20);
-        $pdf->SetFont("", "B");
-        $pdf->Cell(0, $height, "Institut Saint-Dominique - Section secondaire", 0, 1, "C");
-        $pdf->SetFont("", "");
-        $pdf->MultiCell(0, $height, "Rue Caporal Claes, 38 - 1030 Bruxelles\nTel.: 02/240.14.10 - Fax: 02/240.16.11", 0, "C");
-        $email="saintdominique@ens.irisnet.be";
-        $site = "www.saintdominique.be";
-        $width = $pdf->GetStringWidth("$email - $site");
-        $pdf->SetX($pdf->GetX() + ($pageWidth - $pdf->getX() - $rightMargin - $width)/2);
-        $pdf->Write($height, $email, "mailto:$email");
-        $pdf->Write($height, " - ");
-        $pdf->Write($height, $site, $site);
-        $pdf->Ln();
-        $pdf->Line($pdf->GetX(), $pdf->GetY(), $pdf->GetX()+$contentWidth, $pdf->GetY());
-        $pdf->Ln();
-        $pdf->SetY(42);
-        $pdf->SetX(112);
-        $tutor = $student->tutor;
-        $address = $student->address;
-        $zip = $student->zip;
-        $city = $student->city;
-        $pdf->MultiCell(0, $height, utf8_decode("$tutor\n$address\n$zip - $city"));
-        $date = date("j ").utf8_decode(self::$months[date("n")]).date(" Y");
-        $pdf->SetY(75);
-        $pdf->Cell(0, $height, "Schaerbeek, le $date", 0, 1, "R");
-        $name=$student->name;
-        $pdf->SetY(90);
-        $pdf->MultiCell(0, $height, utf8_decode("Chers parents, cher élève,
- 
+            $pdf->AddPage();
+            $pdf->Image($this->webDir("img/logo.png"), $pdf->getX(), $pdf->getY(), 20, 20);
+            $pdf->SetFont("", "B");
+            $pdf->Cell(0, $height, "Institut Saint-Dominique - Section secondaire", 0, 1, "C");
+            $pdf->SetFont("", "");
+            $pdf->MultiCell(0, $height, "Rue Caporal Claes, 38 - 1030 Bruxelles\nTel.: 02/240.14.10 - Fax: 02/240.16.11", 0, "C");
+            $email="saintdominique@ens.irisnet.be";
+            $site = "www.saintdominique.be";
+            $width = $pdf->GetStringWidth("$email - $site");
+            $pdf->SetX($pdf->GetX() + ($pageWidth - $pdf->getX() - $rightMargin - $width)/2);
+            $pdf->Write($height, $email, "mailto:$email");
+            $pdf->Write($height, " - ");
+            $pdf->Write($height, $site, $site);
+            $pdf->Ln();
+            $pdf->Line($pdf->GetX(), $pdf->GetY(), $pdf->GetX()+$contentWidth, $pdf->GetY());
+            $pdf->Ln();
+            $pdf->SetY(42);
+            $pdf->SetX(112);
+            $tutor = $student->tutor;
+            $address = $student->address;
+            $zip = $student->zip;
+            $city = $student->city;
+            $pdf->MultiCell(0, $height, utf8_decode("$tutor\n$address\n$zip - $city"));
+            $date = date("j ").utf8_decode(self::$months[date("n")]).date(" Y");
+            $pdf->SetY(75);
+            $pdf->Cell(0, $height, "Schaerbeek, le $date", 0, 1, "R");
+            $name=$student->name;
+            $pdf->SetY(90);
+            $pdf->MultiCell(0, $height, utf8_decode("Chers parents, cher élève,
+     
 
-"));
-        $pdf->Write($height, utf8_decode("Vous trouverez ci-joint les résultats des travaux de vacances et remise(s) à niveau de "));
-        $pdf->SetFont("", "B");
-        $pdf->Write($height, utf8_decode($name));
-        $pdf->SetFont("", "");
-        $pdf->Write($height, ".\n");
-        $pdf->MultiCell(0, $height, utf8_decode("
+    "));
+            $pdf->Write($height, utf8_decode("Vous trouverez ci-joint les résultats des travaux de vacances et remise(s) à niveau de "));
+            $pdf->SetFont("", "B");
+            $pdf->Write($height, utf8_decode($name));
+            $pdf->SetFont("", "");
+            $pdf->Write($height, ".\n");
+            $pdf->MultiCell(0, $height, utf8_decode("
 Si votre $name est en réussite, le contrat a été rempli et nous considérons que les lacunes observées en juin ont été totalement ou partiellement levées (voir commentaire éventuel laissé par le professeur).
 
 En cas d'échec, par contre, nous encourageons l'élève à poursuivre le travail de remédiation dans les matières concernées. En effet, vous savez que l'accumulation d'échecs dans une même discipline compromet le bon déroulement des apprentissages.
@@ -187,106 +187,106 @@ Valériane Wiot (Directrice-Adjointe) et Vincent Sterpin (Directeur)
 "));
         }
         if ($what == self::ALL || $what == self::TUTORS || $what == self::GROUPED) {
-        $pdf->AddPage();
-        $pageHeader = function() use ($pdf, $height, $schoolyear, $student)
-        {
-            $pdf->SetFont("", "B", 16);
-            $pdf->Cell(0, $height, utf8_decode("Fiche de résultats $schoolyear: ".$student->name." [".$student->class->code."]"), 0, 1, "C");
-            $pdf->Ln();
-            $pdf->SetFont("", "", 12);
-        };
-        $pageHeader();
-        foreach ($works as $w) {
-            $content="";
-            $tdv = $w->isTdv();
-            $type = $tdv ? "TRAVAIL DE VACANCES":"REMISE À NIVEAU";
-            $subject = $w->subject->description." [".$w->subject->code."]";
-            $teacherName = $w->teacher->fullname;
-            if ($w->result) {
-                $result = $w->result;
-                if (!$tdv) {
-                    $result.="/100";
-                }
-                $hasResult = TRUE;
-            } else {
-                 $result=$tdv?"NON-RENDU":"ABSENT";
-                 $hasResult = FALSE;
-            }
-            $hasRemark = $w->remark!==NULL && $w->remark!="";
-            if ($hasRemark) {
-                $remark = rtrim($w->remark);
-                $linesCount = 7 + substr_count($remark,PHP_EOL) +1;
-            } else {
-                $linesCount = 5;
-            }
-            $remainingSpace = $pageHeight - $pdf->GetY() - $rightMargin;
-            $expectedHeight = $linesCount * $height + 2; //+2 to account for border
-            $mustBreak = $remainingSpace <= $expectedHeight;
-            //dump(sprintf("remaining space: %d, lines count %d, expectedHeight: %d, must break: %d, content:\n%s", $remainingSpace, $linesCount, $expectedHeight, $mustBreak, $content));
-            if($mustBreak) {
-                $pdf->AddPage();
-                $pageHeader();
-            }
-            $top = $pdf->GetY();
-            $pdf->SetFont("", "B");
-            $pdf->Write($height, utf8_decode($type));
-            $pdf->SetFont("", "");
-            $pdf->Write($height, utf8_decode(" en $subject"));
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Write($height, utf8_decode("Professeur: $teacherName"));
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Write($height, utf8_decode("Résultat: "));
-            if (!$hasResult) {
-                $pdf->SetFont("", "B");
-            }
-            $pdf->Write($height, $result);
-            $pdf->SetFont("", "");
-            $pdf->Ln();
-            if ($hasRemark) {
+            $pdf->AddPage();
+            $pageHeader = function() use ($pdf, $height, $schoolyear, $student)
+            {
+                $pdf->SetFont("", "B", 16);
+                $pdf->Cell(0, $height, utf8_decode("Fiche de résultats $schoolyear: ".$student->name." [".$student->class->code."]"), 0, 1, "C");
                 $pdf->Ln();
-                $pdf->MultiCell(0, $height, utf8_decode("Commentaire du professeur:\n$remark"));
+                $pdf->SetFont("", "", 12);
+            };
+            $pageHeader();
+            foreach ($works as $w) {
+                $content="";
+                $tdv = $w->isTdv();
+                $type = $tdv ? "TRAVAIL DE VACANCES":"REMISE À NIVEAU";
+                $subject = $w->subject->description." [".$w->subject->code."]";
+                $teacherName = $w->teacher->fullname;
+                if ($w->result) {
+                    $result = $w->result;
+                    if (!$tdv) {
+                        $result.="/100";
+                    }
+                    $hasResult = TRUE;
+                } else {
+                     $result=$tdv?"NON-RENDU":"ABSENT";
+                     $hasResult = FALSE;
+                }
+                $hasRemark = $w->remark!==NULL && $w->remark!="";
+                if ($hasRemark) {
+                    $remark = rtrim($w->remark);
+                    $linesCount = 7 + substr_count($remark,PHP_EOL) +1;
+                } else {
+                    $linesCount = 5;
+                }
+                $remainingSpace = $pageHeight - $pdf->GetY() - $rightMargin;
+                $expectedHeight = $linesCount * $height + 2; //+2 to account for border
+                $mustBreak = $remainingSpace <= $expectedHeight;
+                //dump(sprintf("remaining space: %d, lines count %d, expectedHeight: %d, must break: %d, content:\n%s", $remainingSpace, $linesCount, $expectedHeight, $mustBreak, $content));
+                if($mustBreak) {
+                    $pdf->AddPage();
+                    $pageHeader();
+                }
+                $top = $pdf->GetY();
+                $pdf->SetFont("", "B");
+                $pdf->Write($height, utf8_decode($type));
+                $pdf->SetFont("", "");
+                $pdf->Write($height, utf8_decode(" en $subject"));
+                $pdf->Ln();
+                $pdf->Ln();
+                $pdf->Write($height, utf8_decode("Professeur: $teacherName"));
+                $pdf->Ln();
+                $pdf->Ln();
+                $pdf->Write($height, utf8_decode("Résultat: "));
+                if (!$hasResult) {
+                    $pdf->SetFont("", "B");
+                }
+                $pdf->Write($height, $result);
+                $pdf->SetFont("", "");
+                $pdf->Ln();
+                if ($hasRemark) {
+                    $pdf->Ln();
+                    $pdf->MultiCell(0, $height, utf8_decode("Commentaire du professeur:\n$remark"));
+                }
+                $pdf->Rect($rightMargin, $top - 1, $contentWidth, $pdf->GetY() - $top + 2);
+                $pdf->Ln();
             }
-            $pdf->Rect($rightMargin, $top - 1, $contentWidth, $pdf->GetY() - $top + 2);
-            $pdf->Ln();
-        }
         }
         if ($what == self::ALL || $what == self::INDIVIDUAL) {
-        foreach ($works as $w) {
-            $pdf->AddPage();
-            $pdf->SetFont("", "B");
-            $pdf->Write($height, utf8_decode("Fiche de résultat $schoolyear: ".$w->student->name." [".$w->student->class->code."]"));
-            $pdf->Ln();
-            $tdv = $w->isTdv();
-            $type = $tdv ? "Travail de vacances":"Remise à niveau";
-            $subject = $w->subject->description." [".$w->subject->code."]";
-            $pdf->SetFont("", "");
-            $pdf->Ln();
-            $pdf->Write($height, utf8_decode("$type en $subject"));
-            $pdf->Ln();
-            $pdf->Ln();
-            $teacherName = $w->teacher->fullname;
-            $pdf->Write($height, utf8_decode("Professeur: $teacherName"));
-            $pdf->Ln();
-            $pdf->Ln();
-            $pdf->Write($height, utf8_decode("Résultat: "));
-            if ($w->result) {
-                $pdf->Write($height, $w->result);
-                if (!$tdv) {
-                    $pdf->Write($height, "/100");
+            foreach ($works as $w) {
+                $pdf->AddPage();
+                $pdf->SetFont("", "B");
+                $pdf->Write($height, utf8_decode("Fiche de résultat $schoolyear: ".$w->student->name." [".$w->student->class->code."]"));
+                $pdf->Ln();
+                $tdv = $w->isTdv();
+                $type = $tdv ? "Travail de vacances":"Remise à niveau";
+                $subject = $w->subject->description." [".$w->subject->code."]";
+                $pdf->SetFont("", "");
+                $pdf->Ln();
+                $pdf->Write($height, utf8_decode("$type en $subject"));
+                $pdf->Ln();
+                $pdf->Ln();
+                $teacherName = $w->teacher->fullname;
+                $pdf->Write($height, utf8_decode("Professeur: $teacherName"));
+                $pdf->Ln();
+                $pdf->Ln();
+                $pdf->Write($height, utf8_decode("Résultat: "));
+                if ($w->result) {
+                    $pdf->Write($height, $w->result);
+                    if (!$tdv) {
+                        $pdf->Write($height, "/100");
+                    }
+                } else {
+                    $pdf->Write($height, $tdv?"Non rendu":"Absent");
                 }
-            } else {
-                $pdf->Write($height, $tdv?"Non rendu":"Absent");
-            }
-            $pdf->Ln();
-            if ($w->remark!==NULL && $w->remark!="") {
                 $pdf->Ln();
-                $pdf->Write($height, "Commentaire du professeur:");
-                $pdf->Ln();
-                $pdf->MultiCell(0, $height, utf8_decode($w->remark));
+                if ($w->remark!==NULL && $w->remark!="") {
+                    $pdf->Ln();
+                    $pdf->Write($height, "Commentaire du professeur:");
+                    $pdf->Ln();
+                    $pdf->MultiCell(0, $height, utf8_decode($w->remark));
+                }
             }
-        }
         }
     }
 
