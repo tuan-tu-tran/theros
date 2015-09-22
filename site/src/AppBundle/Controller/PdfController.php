0e4ca24d7801781e526c8a7c880de6790b966d98
Tuan-Tu Tran <tuantu.t@gmail.com>
2015-09-22 03:40:53 +0200

    indent

--- site/src/AppBundle/Controller/PdfController.php
+++ site/src/AppBundle/Controller/PdfController.php
@@ -13,27 +13,13 @@ use AppBundle\ResultPdf;
 
 class PdfController extends Controller implements IAdminPage
 {
-    static $months=array(
-        null,
-        "janvier",
-        "février",
-        "mars",
-        "avril",
-        "mai",
-        "juin",
-        "juillet",
-        "août",
-        "septembre",
-        "octobre",
-        "novembre",
-        "décembre",
-    );
+    static $months = array(null, "janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre");
 
     //batch types
-    const TUTORS="tutors";
-    const GROUPED="grouped";
-    const INDIVIDUAL="ind";
-    const ALL="all";
+    const TUTORS = "tutors";
+    const GROUPED = "grouped";
+    const INDIVIDUAL = "ind";
+    const ALL = "all";
 
     //group types
     const GROUP_BY_TEACHER = "byTeacher";
@@ -55,7 +41,7 @@ class PdfController extends Controller implements IAdminPage
         $student->class = new Klass($row);
 
         $works = array();
-        foreach ($db->query("SELECT w_id FROM work WHERE w_st_id = ".$student->id." AND w_has_result") as $row) {
+        foreach ($db->query("SELECT w_id FROM work WHERE w_st_id = " . $student->id . " AND w_has_result") as $row) {
             $works[] = Work::GetFullById($db, $row["w_id"]);
         }
         $schoolyear = $this->getSchoolYear();
@@ -130,14 +116,14 @@ class PdfController extends Controller implements IAdminPage
         $schoolyear = $this->getSchoolYear();
         $byTeacher = $groupBy == self::GROUP_BY_TEACHER;
         $works = Work::GetListBySchoolYear($db, $schoolyear, TRUE, $byTeacher);
-        $groups=array();
+        $groups = array();
         $byId = array();
         foreach ($works as $w) {
             $s = $byTeacher ? $w->teacher : $w->student;
             if (!isset($byId[$s->id])) {
                 $byId[$s->id] = $s;
-                $groups[]=$s;
-                $s->works=array();
+                $groups[] = $s;
+                $s->works = array();
             } else {
                 $s = $byId[$s->id];
             }
@@ -158,11 +144,11 @@ class PdfController extends Controller implements IAdminPage
 
     private function getLengths(&$pageWidth, &$pageHeight, &$rightMargin, &$contentWidth, &$height)
     {
-        $pageWidth=ResultPdf::PAGE_WIDTH;
-        $pageHeight=ResultPdf::PAGE_HEIGHT;
-        $rightMargin=ResultPdf::MARGIN;
-        $contentWidth=ResultPdf::CONTENT_WIDTH;
-        $height=ResultPdf::LINE_HEIGHT;
+        $pageWidth = ResultPdf::PAGE_WIDTH;
+        $pageHeight = ResultPdf::PAGE_HEIGHT;
+        $rightMargin = ResultPdf::MARGIN;
+        $contentWidth = ResultPdf::CONTENT_WIDTH;
+        $height = ResultPdf::LINE_HEIGHT;
     }
 
     private function addResults($pdf, $student, $works, $schoolyear, $what)
@@ -177,13 +163,13 @@ class PdfController extends Controller implements IAdminPage
             $zip = $student->zip;
             $city = $student->city;
             $pdf->MultiCell(0, $height, utf8_decode("$tutor\n$address\n$zip - $city"));
-            $date = date("j ").utf8_decode(self::$months[date("n")]).date(" Y");
+            $date = date("j ") . utf8_decode(self::$months[date("n")]) . date(" Y");
             $pdf->SetY(75);
             $pdf->Cell(0, $height, "Schaerbeek, le $date", 0, 1, "R");
-            $name=$student->name;
+            $name = $student->name;
             $pdf->SetY(90);
             $pdf->MultiCell(0, $height, utf8_decode("Chers parents, cher élève,
-     
+
 
     "));
             $pdf->Write($height, utf8_decode("Vous trouverez ci-joint les résultats des travaux de vacances et remise(s) à niveau de "));
@@ -210,57 +196,57 @@ Valériane Wiot (Directrice-Adjointe) et Vincent Sterpin (Directeur)
     private function addResultGroup($pdf, $group, $schoolyear, $works, $groupBy)
     {
         $this->getLengths($pageWidth, $pageHeight, $rightMargin, $contentWidth, $height);
-            $byTeacher = $groupBy == self::GROUP_BY_TEACHER;
-            $groupHeader="Fiche de résultats $schoolyear";
-            if ($byTeacher) {
-                $teacher = $group;
-                $groupHeader.= "\nProfesseur: ".$teacher->fullname;
-            } else {
-                $student = $group;
-                $groupHeader.= ": ".$student->name." [".$student->class->code."]";
-            }
-            $groupHeader = utf8_decode($groupHeader);
-            $addPage = function($pdf) use ($height, $schoolyear, $groupHeader)
+        $byTeacher = $groupBy == self::GROUP_BY_TEACHER;
+        $groupHeader = "Fiche de résultats $schoolyear";
+        if ($byTeacher) {
+            $teacher = $group;
+            $groupHeader .= "\nProfesseur: " . $teacher->fullname;
+        } else {
+            $student = $group;
+            $groupHeader .= ": " . $student->name . " [" . $student->class->code . "]";
+        }
+        $groupHeader = utf8_decode($groupHeader);
+        $addPage = function($pdf) use ($height, $schoolyear, $groupHeader)
+        {
+            $pdf->AddPage();
+            $pdf->SetFont("", "B", 16);
+            $pdf->MultiCell(0, $height, $groupHeader, 0, "C");
+            $pdf->Ln();
+            $pdf->SetFont("", "", 12);
+        };
+        $draft = $this->createPdf();
+        $addPage($draft);
+        $actions = array(
+            $addPage
+        );
+        foreach ($works as $w) {
+            $addWorkResult = function($pdf) use ($w, $byTeacher, $height, $rightMargin, $contentWidth)
             {
-                $pdf->AddPage();
-                $pdf->SetFont("", "B", 16);
-                $pdf->MultiCell(0, $height, $groupHeader, 0, "C");
-                $pdf->Ln();
-                $pdf->SetFont("", "", 12);
-            };
-            $draft = $this->createPdf();
-            $addPage($draft);
-            $actions=array(
-                $addPage
-            );
-            foreach ($works as $w) {
-                $addWorkResult = function($pdf) use ($w, $byTeacher, $height, $rightMargin, $contentWidth)
-                {
                 $tdv = $w->isTdv();
-                $type = $tdv ? "TRAVAIL DE VACANCES":"REMISE À NIVEAU";
-                $subject = $w->subject->description." [".$w->subject->code."]";
+                $type = $tdv ? "TRAVAIL DE VACANCES" : "REMISE À NIVEAU";
+                $subject = $w->subject->description . " [" . $w->subject->code . "]";
                 $teacherName = $w->teacher->fullname;
                 if ($w->result) {
                     $result = $w->result;
                     if (!$tdv) {
-                        $result.="/100";
+                        $result .= "/100";
                     }
                     $hasResult = TRUE;
                 } else {
-                     $result=$tdv?"NON-RENDU":"ABSENT";
-                     $hasResult = FALSE;
+                    $result = $tdv ? "NON-RENDU" : "ABSENT";
+                    $hasResult = FALSE;
                 }
-                $hasRemark = $w->remark!==NULL && $w->remark!="";
+                $hasRemark = $w->remark !== NULL && $w->remark != "";
                 if ($hasRemark) {
                     $remark = rtrim($w->remark);
-                    $linesCount = 7 + substr_count($remark,PHP_EOL) +1;
+                    $linesCount = 7 + substr_count($remark, PHP_EOL) + 1;
                 } else {
                     $linesCount = 5;
                 }
-                $top = $pdf->GetY()-1;
+                $top = $pdf->GetY() - 1;
                 $pdf->SetFont("", "B");
                 if ($byTeacher) {
-                    $pdf->Write($height, utf8_decode($w->student->name." [".$w->student->class->code."]"));
+                    $pdf->Write($height, utf8_decode($w->student->name . " [" . $w->student->class->code . "]"));
                     $pdf->Ln();
                     $pdf->Ln();
                 }
@@ -270,9 +256,9 @@ Valériane Wiot (Directrice-Adjointe) et Vincent Sterpin (Directeur)
                 $pdf->Ln();
                 $pdf->Ln();
                 if (!$byTeacher) {
-                $pdf->Write($height, utf8_decode("Professeur: $teacherName"));
-                $pdf->Ln();
-                $pdf->Ln();
+                    $pdf->Write($height, utf8_decode("Professeur: $teacherName"));
+                    $pdf->Ln();
+                    $pdf->Ln();
                 }
                 $pdf->Write($height, utf8_decode("Résultat: "));
                 if (!$hasResult) {
@@ -285,25 +271,25 @@ Valériane Wiot (Directrice-Adjointe) et Vincent Sterpin (Directeur)
                     $pdf->Ln();
                     $comment = "Commentaire";
                     if (!$byTeacher) {
-                        $comment.=" du professeur";
+                        $comment .= " du professeur";
                     }
                     $pdf->MultiCell(0, $height, utf8_decode("$comment:\n$remark"));
                 }
                 $pdf->Rect($rightMargin, $top, $contentWidth, $pdf->GetY() - $top + 2);
-                $bottom = $pdf->GetY()+1;
+                $bottom = $pdf->GetY() + 1;
                 $pdf->Ln();
                 return $bottom < $top; //new page
-                };
-                if ($addWorkResult($draft)) {
-                    $addPage($draft);
-                    $actions[] = $addPage;
-                    $addWorkResult($draft);
-                }
-                $actions[]=$addWorkResult;
+            };
+            if ($addWorkResult($draft)) {
+                $addPage($draft);
+                $actions[] = $addPage;
+                $addWorkResult($draft);
             }
-            foreach($actions as $a) {
-                $a($pdf);
-            }
+            $actions[] = $addWorkResult;
+        }
+        foreach ($actions as $a) {
+            $a($pdf);
+        }
     }
 
     private function addIndividualResults($pdf, $what, $works, $schoolyear)
@@ -313,11 +299,11 @@ Valériane Wiot (Directrice-Adjointe) et Vincent Sterpin (Directeur)
             foreach ($works as $w) {
                 $pdf->AddPage();
                 $pdf->SetFont("", "B");
-                $pdf->Write($height, utf8_decode("Fiche de résultat $schoolyear: ".$w->student->name." [".$w->student->class->code."]"));
+                $pdf->Write($height, utf8_decode("Fiche de résultat $schoolyear: " . $w->student->name . " [" . $w->student->class->code . "]"));
                 $pdf->Ln();
                 $tdv = $w->isTdv();
-                $type = $tdv ? "Travail de vacances":"Remise à niveau";
-                $subject = $w->subject->description." [".$w->subject->code."]";
+                $type = $tdv ? "Travail de vacances" : "Remise à niveau";
+                $subject = $w->subject->description . " [" . $w->subject->code . "]";
                 $pdf->SetFont("", "");
                 $pdf->Ln();
                 $pdf->Write($height, utf8_decode("$type en $subject"));
@@ -334,10 +320,10 @@ Valériane Wiot (Directrice-Adjointe) et Vincent Sterpin (Directeur)
                         $pdf->Write($height, "/100");
                     }
                 } else {
-                    $pdf->Write($height, $tdv?"Non rendu":"Absent");
+                    $pdf->Write($height, $tdv ? "Non rendu" : "Absent");
                 }
                 $pdf->Ln();
-                if ($w->remark!==NULL && $w->remark!="") {
+                if ($w->remark !== NULL && $w->remark != "") {
                     $pdf->Ln();
                     $pdf->Write($height, "Commentaire du professeur:");
                     $pdf->Ln();
@@ -350,8 +336,8 @@ Valériane Wiot (Directrice-Adjointe) et Vincent Sterpin (Directeur)
     private function renderPdf(FPDF $pdf)
     {
         $response = new Response();
-        $response->headers->set("Content-Type","application/pdf");
-        $response->setContent($pdf->Output(null,"S"));
+        $response->headers->set("Content-Type", "application/pdf");
+        $response->setContent($pdf->Output(null, "S"));
         return $response;
     }
 
