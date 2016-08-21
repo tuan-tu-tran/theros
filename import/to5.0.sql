f73ae5fbc7ab79ad48cb827b2205bc737b92522b
Tuan-Tu Tran <tuantu.t@gmail.com>
2016-08-21 23:01:01 +0200

    take schoolyear into account for raw data

---
+++ import/to5.0.sql
@@ -0,0 +1,9 @@
+ALTER TABLE `raw_data` 
+ADD COLUMN `rd_sy_id` INT NOT NULL DEFAULT 1 AFTER `rd_treated`,
+ADD INDEX `fk_raw_data_3_idx` (`rd_sy_id` ASC),
+ADD CONSTRAINT `fk_raw_data_3`
+  FOREIGN KEY (`rd_sy_id`)
+  REFERENCES `schoolyear` (`sy_id`)
+  ON DELETE RESTRICT
+  ON UPDATE RESTRICT;
+
